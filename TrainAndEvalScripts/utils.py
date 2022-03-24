0be6ad3161106e43d622cbe56eb62e10cde1f84c
Jinxuan <i@cjxol.com>
2022-03-24 12:37:38 +0000

    Update playground

--- TrainAndEvalScripts/utils.py
+++ TrainAndEvalScripts/utils.py
@@ -7,3 +7,4 @@ def print_generated_sample_outputs(sample_outputs, tokenizer):
 
     for i, sample_output in enumerate(sample_outputs):
         print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+        print(len(sample_output))
